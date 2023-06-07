4ddda25d2fc148d22f38035c2d4dc35049ad57fb
DESKTOP-EFE9CH8\X1 <thaongan.0912@gmail.com>
2023-06-07 22:50:36 +0700

    Add more col to show on http

--- src/api/views.py
+++ src/api/views.py
@@ -20,5 +20,5 @@ from api.models import Tasks
 # class ShowAll(APIView):
 def index(request):
     queryset = Tasks.objects.all()
-    data = [{'id': obj.id, 'name': obj.name} for obj in queryset]
+    data = [{'id': obj.id, 'name': obj.name, 'create_time': obj.create_time, 'status': obj.status} for obj in queryset]
     return HttpResponse(data)
